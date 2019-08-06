9100e1123f34e6dcd31744640a1b5252106c270c
Marcio Diaz <marcio.diaz@gmail.com>
2019-08-06 23:56:09 +0200

    Fix signature and public tmp

--- core/consensus/babe/src/lib.rs
+++ core/consensus/babe/src/lib.rs
@@ -501,7 +501,7 @@ fn check_header<B: BlockT + Sized, C: AuxStore, T>(
 			}
 
 			if let Some(equivocation_proof) = check_equivocation::<
-				_, _, BabeEquivocationProof<B::Header>, _, _
+				_, _, BabeEquivocationProof<B::Header>, _
 			>(
 				client,
 				slot_now,
