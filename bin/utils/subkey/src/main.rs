a9344e11fbaf9837519260f75bd879d24ebac410
NikVolf <nikvolf@gmail.com>
2019-12-26 23:16:36 +0300

    use reeturn value for main

--- bin/utils/subkey/src/main.rs
+++ bin/utils/subkey/src/main.rs
@@ -34,6 +34,7 @@ use std::{
 	convert::{TryInto, TryFrom},
 	io::{stdin, Read},
 	str::FromStr,
+	fmt,
 };
 
 mod vanity;
@@ -231,24 +232,30 @@ fn get_app<'a, 'b>(usage: &'a str) -> App<'a, 'b> {
 		])
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
 	let usage = get_usage();
 	let matches = get_app(&usage).get_matches();
 
 	if matches.is_present("ed25519") {
-		return execute_proc::<Ed25519>(matches);
+		return execute::<Ed25519>(matches);
 	}
 	if matches.is_present("secp256k1") {
-		return execute_proc::<Ecdsa>(matches)
+		return execute::<Ecdsa>(matches)
 	}
-	return execute_proc::<Sr25519>(matches)
+	return execute::<Sr25519>(matches)
 }
 
-#[derive(Debug, derive_more::Display, derive_more::From)]
+#[derive(derive_more::Display, derive_more::From)]
 enum Error {
 	Static(&'static str),
 	Io(std::io::Error),
 	Formatted(String),
+}
+
+impl fmt::Debug for Error {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		fmt::Display::fmt(self, f)
+	}
 }
 
 fn execute_proc<C: Crypto>(matches: ArgMatches)
@@ -332,7 +339,7 @@ where
 				.value_of("pattern")
 				.map(str::to_string)
 				.unwrap_or_default();
-			let result = vanity::generate_key::<C>(&desired).expect("Key generation failed");
+			let result = vanity::generate_key::<C>(&desired)?;
 			let formated_seed = format_seed::<C>(result.seed);
 			C::print_from_uri(&formated_seed, None, maybe_network);
 		}
